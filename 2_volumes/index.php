b435627b1a4a4b9f9a55e3755643f6d965496dad
Mileno <mileno.alexandre86@gmail.com>
2026-05-03 20:12:42 -0300

    uploading archives for repo

---
+++ 2_volumes/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mensagens</title>
+</head>
+<body>
+    <h1>Escreva sua mensagem</h1>
+    <form action="process.php" method="POST">
+    <input type="text" name="message" id="message" placeholder="Digite sua mensagem aqui">
+    <button type="submit">Salvar</button>
+</form>
+</body>
+</html>
